dc82e6a3e525bf994fba6e2a87f438c8b8d70906
Varun Sadasivam <sadavar05@gmail.com>
2024-08-16 16:33:38 -0500

    changes

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,5 +1,5 @@
 const config = {
-  defaultTitle: 'Varun Sadasivam Portfolio',
+  defaultTitle: 'Varun Sadasivam',
   url: process.env.NEXT_PUBLIC_PORTFOLIO_URL!,
   defaultDescription: 'Portfolio Website',
   googleAnalyticsID: 'G-PE8QW0ZGTZ',
