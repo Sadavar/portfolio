5e0f12db6a4d8e2867359e71de21f19ce25d633e
Varun Sadasivam <sadavar05@gmail.com>
2024-11-20 18:16:48 -0500

    resume link update

--- src/components/ui/theme/Header/Navbar/index.tsx
+++ src/components/ui/theme/Header/Navbar/index.tsx
@@ -4,43 +4,43 @@ import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 
 function Navbar() {
-	const pathname = usePathname();
+  const pathname = usePathname();
 
-	return (
-		<>
-			<div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center pt-2">
-				<div className="bg-black rounded-full p-1">
-					<div className="bg-black space-x-5 border p-5 border-zinc-700 rounded-full w-full sm:w-[600px] flex items-center justify-center">
-						<Link
-							href={`${pathname}#about`}
-							className="text-white hover:text-zinc-300 transition-colors duration-75"
-							scroll
-						>
-							About
-						</Link>
-						<Link
-							href={`${pathname}#projects`}
-							className="text-white hover:text-zinc-300 transition-colors duration-75"
-							scroll
-						>
-							Projects
-						</Link>
-						<a target="_blank" href="https://drive.google.com/file/d/187N-0BK8aSQUxLxObKFgurx5h6t40RCJ/view?usp=sharing" rel="noopener noreferrer"
-							className="text-white hover:text-zinc-300 transition-colors duration-75"
-						>
-							Resume
-						</a>
-						<a target="_blank" href="https://linkedin.com/in/varunsadasivam" rel="noopener noreferrer">
-							<img src="/assets/social/linkedin.png" alt="LinkedIn" className="h-5 w-5" />
-						</a>
-						<a target="_blank" href="https://github.com/sadavar" rel="noopener noreferrer">
-							<img src="/assets/social/github.svg" alt="GitHub" className="h-5 w-5" />
-						</a>
-					</div>
-				</div>
-			</div>
-		</>
-	);
+  return (
+    <>
+      <div className="fixed top-0 left-0 right-0 z-50 flex items-center justify-center pt-2">
+        <div className="bg-black rounded-full p-1">
+          <div className="bg-black space-x-5 border p-5 border-zinc-700 rounded-full w-full sm:w-[600px] flex items-center justify-center">
+            <Link
+              href={`${pathname}#about`}
+              className="text-white hover:text-zinc-300 transition-colors duration-75"
+              scroll
+            >
+              About
+            </Link>
+            <Link
+              href={`${pathname}#projects`}
+              className="text-white hover:text-zinc-300 transition-colors duration-75"
+              scroll
+            >
+              Projects
+            </Link>
+            <a target="_blank" href="https://drive.google.com/file/d/1gCnHwZUa0zoFsK9OQcN0CQH-aQsMNFFl/view" rel="noopener noreferrer"
+              className="text-white hover:text-zinc-300 transition-colors duration-75"
+            >
+              Resume
+            </a>
+            <a target="_blank" href="https://linkedin.com/in/varunsadasivam" rel="noopener noreferrer">
+              <img src="/assets/social/linkedin.png" alt="LinkedIn" className="h-5 w-5" />
+            </a>
+            <a target="_blank" href="https://github.com/sadavar" rel="noopener noreferrer">
+              <img src="/assets/social/github.svg" alt="GitHub" className="h-5 w-5" />
+            </a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Navbar;
